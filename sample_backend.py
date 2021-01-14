2ba5b4d62326338802e53217c59fd1e0cdc1d3ea
eddyiwnl <ed.du.email@gmail.com>
2021-01-13 18:15:44 -0800

    Part 4 task 3

--- sample_backend.py
+++ sample_backend.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import request
 from flask import jsonify
 from flask_cors import CORS
+import random, string
 
 app = Flask(__name__)
 CORS(app)
@@ -39,9 +40,16 @@ def get_users():
         return users
     elif request.method == 'POST':
         userToAdd = request.get_json()
+        if(userToAdd.get('id') is None):
+            id_chars = string.ascii_letters + string.digits
+            user_id = []
+            for i in range(6):
+                user_id.append(random.choice(id_chars))
+        user_id_str = ''.join(user_id)
+        userToAdd['id'] = user_id_str
         users['users_list'].append(userToAdd)
         resp = jsonify(success=True)
-        #resp.status_code = 201 #optionally, you can always set a response code.
+        resp.status_code = 201 #optionally, you can always set a response code.
         # 200 is the default code for a normal response
         return resp
     elif request.method == 'DELETE':
